eba34396307b6c3028d664859fc272a4bf144285
CalebAlfredS <85428170+CalebAlfredS@users.noreply.github.com>
2022-05-05 13:57:11 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "CONSTANT";
+const accessCode2 = "ARRAY";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("T C S A O N T", 100,50)
+    fill("lightblue")
+    text("Hint: it does not change at all !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("R R Y A", 700,150)
+    fill("lightblue")
+    text("Hint: It is used to store multiple elements !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+}
